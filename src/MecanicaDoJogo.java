cb1c71730a9b4daf184b78a118cb3f995b4097c3
Danilo Couto <danilocoutodev@gmail.com>
2024-04-12 14:36:43 -0300

    feat: interfaces

---
+++ src/MecanicaDoJogo.java
@@ -0,0 +1,10 @@
+public interface MecanicaDoJogo {
+  void inicia();
+  public boolean fazPerguntaEAvalia(String palavra, String quiz);
+}
+
+/*
+interface que representa o andamento e a lógica do jogo. É responsável por ditar o andamento do jogo.
+Ela que vai dizer se o jogo acabou ou não, se o usuário acertou a
+    palavra ou não, se o usuário pode tentar acertar a palavra novamente e qual foi a pontuação final
+do jogador. Pelo menos duas implementações dessa interface devem ser criadas.*/
